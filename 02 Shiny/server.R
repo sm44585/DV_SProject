71d6ff4f51e1e5d48aba2f2ba17c9993930562ef
Spencer Muncey <scmuncey@yahoo.com>
2015-11-18 11:50:34 -0600

    Updated server and UI. may have to delete your server and ui script - otherwise it will give you an error

--- 02 Shiny/server.R
+++ 02 Shiny/server.R
@@ -6,17 +6,28 @@ require(dplyr)
 require(shiny)
 
 shinyServer(function(input, output) {
+  #Code to generate data frame
+  vehicles <- eventReactive(input$redoPlot, {
+    vehicles <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select ATVTYPE,BARRELS08,BARRELSA08,CITY08,CITYA08,CO2TAILPIPEAGPM,CO2TAILPIPEGPM,COMB08,COMBA08,CYLINDERS,FUELCOST08,FUELCOSTA08,FUELTYPE,FUELTYPE1,FUELTYPE2,HIGHWAY08,HIGHWAYA08,HLV,HPV,LV2,LV4,MPGDATA,PV2,PV4,YEAR,MAKE,TRANY from VEHICLES"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_sm44585', PASS='orcl_sm44585', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+  }, ignoreNULL = FALSE)
   
-  output$distPlot <- renderPlot({
-    # Start your code here.
-    
-    MPG_PV2_KPI_LOW = input$KPI1   
-    MPG_PV2_KPI_HIGH = input$KPI2
-    # Loads the data from Vehicle data set into the Vehicle dataframe
-    # Change the USER and PASS below to be your UTEid
-vehicles <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select ATVTYPE,BARRELS08,BARRELSA08,CITY08,CITYA08,CO2TAILPIPEAGPM,CO2TAILPIPEGPM,COMB08,COMBA08,CYLINDERS,FUELCOST08,FUELCOSTA08,FUELTYPE,FUELTYPE1,FUELTYPE2,HIGHWAY08,HIGHWAYA08,HLV,HPV,LV2,LV4,MPGDATA,PV2,PV4,YEAR,MAKE from VEHICLES"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_sm44585', PASS='orcl_sm44585', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE),))
-    # The following is equivalent to creat a crosstab with two KPIs in Tableau"
-    crosstab <- vehicles %>% group_by(MAKE, YEAR) %>% summarize(sum_comb08 = sum(COMB08), sum_pv2 = sum(PV2),sum_pv4 = sum(PV4)) %>% mutate(ratio_1 = sum_comb08 / (sum_pv2))%>% mutate(ratio_2 = sum_comb08 / (sum_pv4)) %>% mutate(kpi_1 = ifelse(ratio_1 < MPG_PV2_KPI_LOW, '03 Not Efficient or Spacious', ifelse(ratio_1 <= MPG_PV2_KPI_HIGH, '02 Average Efficiency and Space', '01 Efficient and Spacious')))%>% mutate(kpi_2 = ifelse(ratio_2 < MPG_PV2_KPI_LOW, '03 Not Efficient or Spacious', ifelse(ratio_2 <= MPG_PV2_KPI_HIGH, '02 Average Efficiency and Space', '01 Efficient and Spacious'))) %>%filter(MAKE %in% c("Acura", "Aston Martin", "Audi", "Bentley", "BMW", "Buick", "Chevrolet", "Dodge", "Ferrari", "Ford", "Honda", "Kia", "Lincoln", "Lexus", "Maserati", "Mazda", "Mercedes-Benz", "Nissan", "Toyota", "Volkswagen")) %>% filter(ratio_1 != Inf, ratio_2 != Inf)
+  #Code that generates reactive transmission filter for the bar chart.
+  trans_filter <- eventReactive(input$redoPlot, {
+    if (input$TRANY == "All"){
+      trans_filter = c("Automatic 3-spd", "Automatic 4-spd","Automatic 5-spd","Automatic 6-spd","Automatic 6spd","Automatic 7-spd", "Automatic 8-spd", "Automatic 9-spd", "Manual 3-spd", "Manual 4-spd", "Manual 5-spd", "Manual 5 spd", "Manual 6-spd", "Manual 7-spd")
+    }
+    else {
+      trans_filter = input$TRANY
+    }
+    }, ignoreNULL = FALSE)
+  
+  #Code that generates reactive KPI inputs for both crosstabs
+  MPG_PV2_KPI_LOW <- eventReactive(input$redoPlot, {MPG_PV2_KPI_LOW = input$KPI1}, ignoreNULL = FALSE)   
+  MPG_PV2_KPI_HIGH <- eventReactive(input$redoPlot, {MPG_PV2_KPI_HIGH = input$KPI2}, ignoreNULL = FALSE)
+  
+  #Code to generate PV2 Crosstab plot
+  output$crosstabPV2Plot <- renderPlot({
+    crosstab <- vehicles() %>% group_by(MAKE, YEAR) %>% summarize(sum_comb08 = sum(COMB08), sum_pv2 = sum(PV2),sum_pv4 = sum(PV4)) %>% mutate(ratio_1 = sum_comb08 / (sum_pv2))%>% mutate(ratio_2 = sum_comb08 / (sum_pv4)) %>% mutate(kpi_1 = ifelse(ratio_1 < MPG_PV2_KPI_LOW(), '03 Not Efficient or Spacious', ifelse(ratio_1 <= MPG_PV2_KPI_HIGH(), '02 Average Efficiency and Space', '01 Efficient and Spacious')))%>% mutate(kpi_2 = ifelse(ratio_2 < MPG_PV2_KPI_LOW(), '03 Not Efficient or Spacious', ifelse(ratio_2 <= MPG_PV2_KPI_HIGH(), '02 Average Efficiency and Space', '01 Efficient and Spacious'))) %>%filter(MAKE %in% c("Acura", "Aston Martin", "Audi", "Bentley", "BMW", "Buick", "Chevrolet", "Dodge", "Ferrari", "Ford", "Honda", "Kia", "Lincoln", "Lexus", "Maserati", "Mazda", "Mercedes-Benz", "Nissan", "Toyota", "Volkswagen")) %>% filter(ratio_1 != Inf, ratio_2 != Inf)
     
     # This line turns the make and year columns into ordered factors.
     crosstab <- crosstab %>% transform(MAKE = ordered(MAKE), YEAR = ordered(YEAR))
@@ -45,6 +56,49 @@ plot <-ggplot() +
             position=position_identity()
       ) 
     # End your code here.
+    return(plot)
+  })
+  
+  #Code to generate Bar Chart Plot
+  output$barchartPlot <- renderPlot({
+    bar_chart <- vehicles() %>% select(TRANY, HIGHWAY08, CITY08) %>% subset(TRANY %in% trans_filter()) %>% group_by(TRANY) %>% summarise(avg_city_MPG = mean(CITY08), avg_highway_MPG = mean(HIGHWAY08)) %>% melt(id.vars = c("TRANY")) %>% group_by(variable) %>% mutate(WINDOW_AVG_MPG = mean(value))
+      #Plot Function to generate bar chart with reference line and values
+      plot <- ggplot() + 
+      coord_cartesian() + 
+      scale_x_discrete() +
+      scale_y_continuous() +
+      facet_wrap(~variable) +
+      labs(title='Average Highway and City MPG based on transmission ') +
+      labs(x=paste("Transmission"), y=paste("MPG")) +
+      layer(data=bar_chart, 
+            mapping=aes(x=TRANY, y=value), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="bar",
+            geom_params=list(colour="blue", fill="white"), 
+            position=position_dodge()
+      ) + coord_flip() + 
+      layer(data=bar_chart, 
+            mapping=aes(x=TRANY, y=value, label=round(WINDOW_AVG_MPG, 2)), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="text",
+            geom_params=list(colour="black", hjust=2), 
+            position=position_identity()
+      ) +
+      layer(data=bar_chart, 
+            mapping=aes(yintercept = WINDOW_AVG_MPG), 
+            geom="hline",
+            geom_params=list(colour="red")
+      ) +
+      layer(data=bar_chart, 
+            mapping=aes(x=TRANY, y=value, label=round(value, 2)), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="text",
+            geom_params=list(colour="black", hjust=0), 
+            position=position_identity()
+      )
     return(plot)
   })
 })
--- 02 Shiny/ui.R
+++ 02 Shiny/ui.R
@@ -6,24 +6,42 @@ library(shiny)
 shinyUI(pageWithSidebar(
   
   # Application title
-  headerPanel("PV2 Crosstab"),
+  titlePanel("Project 6: Shiny app to display MPG data", windowTitle = "Project 6"),
   
   # Sidebar with a slider input for number of observations
   sidebarPanel(
+    #PV2 Crosstab inputs
+    h3("Crosstab sliders: 2 Door vehicle Passenger Volume"),
+    #Slider for PV2 KPI1
     sliderInput("KPI1", 
-                "KPI_Low_Max_value:", 
+                "KPI Low Max value:", 
                 min = 0,
                 max = 1, 
                 value = 1),
+    #Slider for PV2 KPI2
     sliderInput("KPI2", 
-                "KPI_Medium_Max_value:", 
+                "KPI Medium Max value:", 
                 min = 1,
                 max = 2, 
-                value = 2)
-  ),
+                value = 2),
+    #PV4 Crosstab inputs
+    
+    #Bar Chart inputs
+    h3("Bar Chart filter: City and Highway MPG for each transmission"),
+    #Checkbox for filtering transmission
+    checkboxGroupInput("TRANY",
+      "Transmission:",
+      c("All", "Automatic 3-spd", "Automatic 4-spd","Automatic 5-spd","Automatic 6-spd","Automatic 6spd","Automatic 7-spd", "Automatic 8-spd", "Automatic 9-spd", "Manual 3-spd", "Manual 4-spd", "Manual 5-spd", "Manual 5 spd", "Manual 6-spd", "Manual 7-spd"), "All"
+    ),
+    
+    #action button to generate plots
+    p("Click the button to Generate the plots."),
+    actionButton("redoPlot", "Generate Plot")
+    ),
   
   # Show a plot of the generated distribution
   mainPanel(
-    plotOutput("distPlot")
+    plotOutput("crosstabPV2Plot"),
+    plotOutput("barchartPlot")
   )
 ))
